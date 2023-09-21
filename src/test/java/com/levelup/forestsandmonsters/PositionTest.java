8ffd4ba591a99879189ff9983fd525ed08092044
Kevin Wlosinski <kevinwlosinski@gmail.com>
2023-09-21 15:06:07 +0000

    PositionTest.java class

---
+++ src/test/java/com/levelup/forestsandmonsters/PositionTest.java
@@ -0,0 +1,14 @@
+package com.levelup.forestsandmonsters;
+
+import java.awt.Point;
+
+import org.junit.Test;
+
+public class PositionTest {
+    
+    @Test
+    public void testOriginPosition() {
+        Point position1 = new Point(5, 5);
+        System.out.println("Position: " + position1);
+    }
+}
