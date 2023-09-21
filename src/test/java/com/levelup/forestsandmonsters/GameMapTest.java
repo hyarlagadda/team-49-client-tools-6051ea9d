b1cf21b2077c505ae00ef98d095aa9db35af3953
Peru Mamillapalli <perumamillapalli@gmail.com>
2023-09-21 15:26:32 +0000

    added test

--- src/test/java/com/levelup/forestsandmonsters/GameMapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameMapTest.java
@@ -1,7 +1,15 @@
 package com.levelup.forestsandmonsters;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class GameMapTest {
     
+@Test
+public void CreatePositions(){
 
+    assertTrue(false);
+}
     
 }
