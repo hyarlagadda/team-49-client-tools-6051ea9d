2901c3b2f8c6566cdedd749124f253478197b10c
Peru Mamillapalli <perumamillapalli@gmail.com>
2023-09-21 15:54:59 +0000

    formatted class

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -7,36 +7,29 @@ public class GameMap {
 
     int numPositions =100;
 
-        public GameMap()
-        {
-            CreatePositions();
-        }
+    public GameMap() {
+        CreatePositions();
+    }
 
-        private void CreatePositions()
-        {
-           positions = new Position[10][10];
-            for (int x = 0; x < 10; x++)
-            {
-                for (int y = 0; y < 10; y++)
-                {
-                    Position pos = new Position(x, y);
-                    positions[x][y] = pos;
-                }
-            }
-            startingPosition = positions[0][0];
-        }
-
-
-        public boolean IsPositionValid(Position pos)
-        {
-            if (pos.coordinates.x >= 0 && pos.coordinates.x < 10 && pos.coordinates.y >=0 && pos.coordinates.y < 10)
-            {
-                return true;
-            }
-            else {
-                return false;
+    private void CreatePositions() {
+        positions = new Position[10][10];
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                Position pos = new Position(x, y);
+                positions[x][y] = pos;
             }
         }
+        startingPosition = positions[0][0];
+    }
+
+
+    public boolean IsPositionValid(Position pos) {
+        if (pos.coordinates.x >= 0 && pos.coordinates.x < 10 && pos.coordinates.y >= 0 && pos.coordinates.y < 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 }
