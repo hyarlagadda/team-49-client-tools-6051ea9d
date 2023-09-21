ec734e4eaa293f7b9b9994d05a925219fda7fe3d
Zach Thomas <zachary.thomas@wellsfargo.com>
2023-09-21 15:50:31 +0000

    removed unneeded import

--- src/test/java/com/levelup/forestsandmonsters/PositionTest.java
+++ src/test/java/com/levelup/forestsandmonsters/PositionTest.java
@@ -3,8 +3,6 @@ package com.levelup.forestsandmonsters;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.awt.Point;
-
 import org.junit.Test;
 
 public class PositionTest {
