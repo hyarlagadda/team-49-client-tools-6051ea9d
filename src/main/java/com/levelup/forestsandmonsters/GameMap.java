06b16f05cfd0d6ef1d4c12e2b80c0c635a106593
Harish Yarlagadda <hyarlagadda23@gmail.com>
2023-09-21 15:25:18 +0000

    GameMap updates

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,8 +1,8 @@
 package com.levelup.forestsandmonsters;
 
-public class GampMap {
+public class GameMap {
     
-    public Position[] positions;
+    public Position[][] positions;
     public Position startingPosition;
 
     int numPositions =100;
@@ -14,22 +14,22 @@ public class GampMap {
 
         private void CreatePositions()
         {
-           positions = new Position[10,10];
+           positions = new Position[10][10];
             for (int x = 0; x < 10; x++)
             {
                 for (int y = 0; y < 10; y++)
                 {
                     Position pos = new Position(x, y);
-                    positions[x,y] = pos;
+                    positions[x][y] = pos;
                 }
             }
-            startingPosition = positions[0,0];
+            startingPosition = positions[0][0];
         }
 
 
         public boolean IsPositionValid(Position pos)
         {
-            if (pos.x >= 0 && pos.x < 10 && pos.y >=0 && pos.y < 10)
+            if (pos.coordinates.x >= 0 && pos.coordinates.x < 10 && pos.coordinates.y >=0 && pos.coordinates.y < 10)
             {
                 return true;
             }
