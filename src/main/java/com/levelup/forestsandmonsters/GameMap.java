bb0521bca497d8914ed2bbf63f3d262a43a203b9
Peru Mamillapalli <perumamillapalli@gmail.com>
2023-09-21 15:19:03 +0000

    updated

---
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -0,0 +1,42 @@
+package com.levelup.forestsandmonsters;
+
+public class GampMap {
+    
+    public Position[] positions;
+    public Position startingPosition;
+
+    int numPositions =100;
+
+        public GameMap()
+        {
+            CreatePositions();
+        }
+
+        private void CreatePositions()
+        {
+           positions = new Position[10,10];
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    Position pos = new Position(x, y);
+                    positions[x,y] = pos;
+                }
+            }
+            startingPosition = positions[0,0];
+        }
+
+
+        public boolean IsPositionValid(Position pos)
+        {
+            if (pos.x >= 0 && pos.x < 10 && pos.y >=0 && pos.y < 10)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+}
