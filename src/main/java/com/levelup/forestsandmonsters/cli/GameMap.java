c4a4312036d910d8ba49c09cfb7b68cb4e3b380c
Peru Mamillapalli <perumamillapalli@gmail.com>
2023-09-21 14:57:44 +0000

    GameMap class

---
+++ src/main/java/com/levelup/forestsandmonsters/cli/GameMap.java
@@ -0,0 +1,19 @@
+package com.levelup.forestsandmonsters.cli;
+
+import javax.swing.text.Position;
+
+public class GameMap {
+    
+    public Position positions;
+
+        public GameMap()
+        {
+            //CreatePositions();
+        }
+
+       
+
+
+
+
+}
---
+++ src/test/java/com/levelup/GameMapTest.java
@@ -0,0 +1,7 @@
+package com.levelup;
+
+public class GameMapTest {
+    
+    
+    
+}
