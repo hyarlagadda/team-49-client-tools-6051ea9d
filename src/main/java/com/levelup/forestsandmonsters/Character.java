a29b955530d0780e2a19cc7265c848ff053dc91e
Harish Yarlagadda <hyarlagadda23@gmail.com>
2023-09-21 15:07:45 +0000

    Character updates

--- src/main/java/com/levelup/forestsandmonsters/Character.java
+++ src/main/java/com/levelup/forestsandmonsters/Character.java
@@ -1,5 +1,35 @@
 package com.levelup.forestsandmonsters;
 
+import java.awt.Point;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
 public class Character {
     
+    static String DEFAULT_NAME = "ELEPHANT";
+    private String name;
+
+    public Character() {
+        this.name = DEFAULT_NAME;
+    }
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // public void enterMap(GameMap map) {
+    
+    // }
+
+    // public Position getPosition() {
+
+    //     return null;
+    // }
+
+    public void move(DIRECTION direction) {
+        
+    }
 }
--- src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
+++ src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
@@ -1,5 +1,30 @@
 package com.levelup.forestsandmonsters;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
 public class CharacterTest {
+
+    Character character = new Character();
+
+    @Test
+    public void testDefaultName() {
+        assertEquals(character.getName(), Character.DEFAULT_NAME);
+    }
     
+    @Test
+    public void testGetName() {
+        Character character1 = new Character("CLIENT-TOOLS"); 
+        assertEquals(character1.getName(), "CLIENT-TOOLS");
+    }
+
+    @Test
+    public void testMove() {
+        character.move(DIRECTION.EAST);
+        assertTrue(true);
+    }
 }
