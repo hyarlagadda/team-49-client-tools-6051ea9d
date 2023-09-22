af5f80cf7257670460e5d511b8ff59896ff139f1
Peru Mamillapalli <perumamillapalli@gmail.com>
2023-09-22 13:40:27 +0000

    code refactoring

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -4,6 +4,7 @@ public class GameMap {
     
     public Position[][] positions;
     public Position startingPosition;
+    private static final int maxBoundary = 10;
 
     int numPositions =100;
 
@@ -13,8 +14,8 @@ public class GameMap {
 
     private void createPositions() {
         positions = new Position[10][10];
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < maxBoundary; x++) {
+            for (int y = 0; y < maxBoundary; y++) {
                 Position pos = new Position(x, y);
                 positions[x][y] = pos;
             }
@@ -22,7 +23,8 @@ public class GameMap {
         startingPosition = positions[0][0];
     }
 
-    public boolean isPositionValid(Position pos) {
+
+    public boolean IsPositionValid(Position pos) {
         if (pos.coordinates.x >= 0 && pos.coordinates.x < 10 && pos.coordinates.y >= 0 && pos.coordinates.y < 10) {
             return true;
         } else {
